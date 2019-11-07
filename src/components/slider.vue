e8f21dfac2feff0ceb42a29cd1235b1e59b6553c
gaofan <gaofan19901103@gmail.com>
2019-11-08 00:11:43 +0800

    slider

---
+++ src/components/slider.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="bar">
+        <div class="braille">&#10247;</div>
+        <div><button>test</button></div>
+        <div><button>test1</button></div>
+        <div><p>sdfsdf</p></div>
+    </div> 
+</template>
+
+<script>
+    export default {
+        props:{
+
+        },
+        data() {
+            return {
+                source: {
+                    
+                }
+            };
+        },
+        computed:{
+
+        }
+    };
+</script>
+
+<style lang="less" scoped>
+   .bar{
+       height: 25px;
+       display: flex;
+       overflow: hidden;
+       width: 15px;
+       background-color: red;
+       
+   }
+
+    .bar:hover{
+        width: 200px;
+        transition: width 0.3s ease-in-out;
+    }
+
+   .move{
+       width: 100px;
+   }
+
+   .braille{
+       font-size: 24px;
+       font-weight: 800;
+   }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import App from './components/App.vue';
 import g from './components/g.vue';
 import container from './components/reset-submission-view.vue'; 
+import slider from './components/slider.vue';
 
 require('./directives/test-directive.js');
 
 Vue.component('app', App);
 Vue.component('container', container);
 Vue.component('grid', g);
+Vue.component('slider', slider);
 
 new Vue({
   el: '#app'
