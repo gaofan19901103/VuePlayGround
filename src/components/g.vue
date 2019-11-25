5aeca686d6e8e08c55d71310e63254ae89ff8e3b
gaofan <gaofan19901103@gmail.com>
2019-11-25 22:46:57 +0800

    try v-s

--- src/components/g.vue
+++ src/components/g.vue
@@ -1,28 +1,12 @@
 <template>
   <div class="grid">
-      <div class="header">
-          <div class="column"><span>RUN</span></div>
-          <div class="column"><span>TRADE</span></div>
-          <div class="column"><span>CCY</span></div>
-          <div class="column"><span>PROD</span></div>
-          <div class="column"><span>TYPE</span></div>
-          <div class="column"><span>STATUS</span></div>
-      </div>
-      <div class="row">
-          <div class="column"><span>01-AUG-19</span></div>
-          <div class="column"><span>02-AUG-19</span></div>
-          <div class="column"><span>EUR</span></div>
-          <div class="column"><span>FRA</span></div>
-          <div class="column"><span>3M,6M</span></div>
-          <div class="column"><span>SUBMITTED</span></div>
-      </div>
-      <div class="row">
-          <div class="column"><span>01-AUG-19</span></div>
-          <div class="column"><span>02-AUG-19</span></div>
-          <div class="column"><span>EUR</span></div>
-          <div class="column"><span>FRA</span></div>
-          <div class="column"><span>3M,6M</span></div>
-          <div class="column"><span>SUBMITTED</span></div>
+      
+      <div class="row" v-for="(item, index) in scrollList" v-bind:key="item.key">
+          <div class="column"><span>{{index}} | </span></div>
+          <div class="column"><span>{{item.rt}}</span></div>
+          <div class="column"><span>{{item.vt}}</span></div>
+          <div class="column"><span>{{item.currency}}</span></div>
+          <div class="column"><span>{{item.status}}</span></div>
       </div>
   </div>
 </template>
@@ -40,45 +24,81 @@
 
 
     export default {
+        created: function(){
+            let x = 0;
+        },
+        beforeMount: function(){
+            let x = 0;
+        },
+        mounting: function(){
+            let x = 0;
+        },
+        mounted: function(){
+            let scroller = document.createElement("div");
+            scroller.style.opacity = 0;
+            scroller.style.position = "absolute";
+            scroller.style.top = 0;
+            scroller.style.left = 0;
+            scroller.style.width = "1px";
+            scroller.style.height = 3000 + "px";
+            
+            this.$el.append(scroller);
+
+            let x = 0;
+        },
         props:{
 
         },
         data() {
             return {
-                source: {
-                    
-                }
+                source: [
+                    {id: '1', rt: '01-AUG-19', vt:'02-AUG-19', currency: 'USD', product:'FRA', tenor:'3M', status:'Open'}
+                ]
             };
         },
         computed:{
+            myList: function(){
+                var list = [];
 
+                for(let i = 0; i < 1000; i ++){
+                    list.push({id: i, rt: '01-AUG-19', vt:'02-AUG-19', currency: 'USD', product:'FRA', tenor:'3M', status:'Open'});
+                }
+
+                return list;
+            },
+            scrollList: function(){
+                let index = 300 / 30;
+                return this.myList.slice(0, index);
+            }
         }
     };
 </script>
 
 <style lang="less" scoped>
     .grid{        
+        height: 300px;
         width: 100%;
         background-color: black;
         color: white;
-
-.header{
-                display: flex;
-}
+        overflow: auto;
+        position: relative;
 
         .row{
             display: flex;
+            height: 30px;
         }
 
-        .column{}
-
-        span{
-            font-size: 12px;
-            //font-family: 'NEX Light';
-            background-color: #232323;
-            line-height: 24px;
-            margin: 1px;
-            padding: 4px 8px;
+        .column{
+            
         }
+
+        // span{
+        //     font-size: 12px;
+        //     //font-family: 'NEX Light';
+        //     background-color: #232323;
+        //     line-height: 24px;
+        //     margin: 1px;
+        //     padding: 4px 8px;
+        // }
     }
 </style>
