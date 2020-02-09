c099a16cedbb09e1cc4b2c14cbd2c7b21087a77e
gaofan <gaofan19901103@gmail.com>
2020-02-09 23:53:13 +0800

    fix vs

--- src/components/sheet/sheet.component.vue
+++ src/components/sheet/sheet.component.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
+  <div class="v-sheet-vessel">
+       
     <div class="v-sheet" v-on:scroll="onXX($event)">
-        <grid :virtual-list="virtualList" :grid-top="gridTop"></grid>
-        <selection-area></selection-area>
+        <grid :virtual-list="virtualList" v-bind:grid-top="gridTop"></grid>    
+        
         <div class="vault" :style="{ height: vaultHeight + 'px' }"></div>
     </div>
+
   </div>
 </template>
 
@@ -50,7 +52,7 @@
                 let visibleRowCount = Math.ceil(this.sheetHeight / this.rowHeight);
                 let scrolledRowCount = Math.floor(this.sheetScrollTop / this.rowHeight);          
                 let startIndex = scrolledRowCount  - this.virtualBuffer < 0 ? 0 : scrolledRowCount  - this.virtualBuffer;   
-                let endIndex = startIndex + visibleRowCount + this.virtualBuffer;                      
+                let endIndex = startIndex + visibleRowCount + this.virtualBuffer * 2;                      
                  return this.source.slice(startIndex, endIndex);
             }        
         },
@@ -60,13 +62,11 @@
         methods:{
            onXX: function(e){
               if(Math.abs(e.target.scrollTop -  this.lastVirtualPosition) > this.virtualBuffer * this.rowHeight){
-                    console.log('scroll render');
+                    console.debug('scroll render');
                     let scrollTop = e.target.scrollTop;
-
                     this.lastVirtualPosition = scrollTop;
                     this.sheetScrollTop = scrollTop;
-                    let gridTop = scrollTop - this.virtualBuffer * this.rowHeight;
-                    this.gridTop = gridTop < 0 ? 0 : gridTop;                                        
+                    this.gridTop = (scrollTop - this.virtualBuffer * this.rowHeight) < 0 ? 0 : (scrollTop - this.virtualBuffer * this.rowHeight);
                 }
            }           
         }
@@ -74,32 +74,39 @@
 </script>
 
 <style lang="less" scoped>
-    .v-sheet{        
-        --sheet-font-color: black;
-        --sheet-font-size: 12px;
-        --sheet-background-color: transparent;
-        --vault-height: 0px;
-
+    .v-sheet-vessel{
         height: 100%;
-        max-height: 100%;
         width: 100%;
-        overflow: auto;
-        position: relative;
-
-        font-size: var(--sheet-font-size);
-        background-color: var(--sheet-background-color);
-        color: var(--sheet-font-color);
+        box-sizing: border-box;
 
         *{
             box-sizing: border-box;
-        }   
+        }
         
-        .vault{
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 1px;
-            height: var(--vault-height);            
-        }       
+        .v-sheet{        
+            --sheet-font-color: black;
+            --sheet-font-size: 12px;
+            --sheet-background-color: transparent;
+            --vault-height: 0px;
+
+            position: relative;
+            height: 100%;
+            width: 100%;
+            // max-height: 100%;       
+            // max-width: 100%;
+            overflow: auto;
+            
+            font-size: var(--sheet-font-size);
+            background-color: var(--sheet-background-color);
+            color: var(--sheet-font-color);
+                      
+            .vault{
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 1px;
+                height: var(--vault-height);            
+            }       
+        }
     }
 </style>
