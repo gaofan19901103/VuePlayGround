26c88b93d99d17b83d868bda9e143139cb0fa195
Gao Fan <gaofan19901103@gmail.com>
2020-04-05 01:12:03 +0800

    clean up selection-area.component

--- src/components/sheet/selection-area.component.vue
+++ src/components/sheet/selection-area.component.vue
@@ -1,28 +1,32 @@
 <template>
     <div class="selection-area-root" v-if="selections.length">
-        <div class="selection-area" v-for="area in selectionAreas" v-bind:key="area.key" :style="area.style"><div class="dot"></div></div>
+        <div class="selection-area" v-for="area in selectionAreas" v-bind:key="area.key" :style="area.style">
+            <div class="drag-dot" v-if="showDragDot"></div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         props:{
-           selections: {type: Array, required: false, default: 0 },
-           indexedRows: {type: Array, required: false, default: [] },
-           indexedCols: {type: Array, required: false, default: [] }
+           selections: { type: Array, required: false, default: [] },
+           indexedRows: { type: Array, required: false, default: [] },
+           indexedCols: { type: Array, required: false, default: [] },
+
+           //GF:Review - the two props below for freeze col and header.
+           noBottomBorder: { type: Boolean, required: false, default: false },
+           noRightBorder: { type: Boolean, required: false, default: false } 
         },
         data: function(){
             return{
-                freezeAreas: [],
-                countOfHeaderRow: this.indexedRows.findIndex(x => !x.isHeader),
-                countOfFreeze: this.indexedCols.findIndex(x => !x.freeze)
+
             }
         },
         computed:{
+            showDragDot: function(){
+                return this.selections.length == 1;
+            },
             selectionAreas:function(){
-
-                this.freezeAreas = [];
-
                 let areas = [];
 
                 this.selections.forEach((slt, index) =>{  
@@ -34,15 +38,15 @@
 
                     areas.push({
                         key: index,
-                        // top: this.indexedRows[tlY].cells[tlX].y,
-                        // left: this.indexedRows[tlY].cells[tlX].x,
-                        // height: this.indexedRows[brY].cells[brX].y - this.indexedRows[tlY].cells[tlX].y + this.indexedRows[brY].height,
-                        // width: this.indexedRows[brY].cells[brX].x - this.indexedRows[tlY].cells[tlX].x + this.indexedCols[brX].width,
                         style: {
-                            top:  this.indexedRows[tlY].cells[tlX].y + 'px',
-                            left: this.indexedRows[tlY].cells[tlX].x + 'px',
-                            height:  (this.indexedRows[brY].cells[brX].y - this.indexedRows[tlY].cells[tlX].y + this.indexedRows[brY].height) + 'px',
-                            width: (this.indexedRows[brY].cells[brX].x - this.indexedRows[tlY].cells[tlX].x + this.indexedCols[brX].width) + 'px'
+                            ...{
+                                top:  this.indexedRows[tlY].y + 'px',
+                                left: this.indexedCols[tlX].x + 'px',
+                                height: this.indexedRows[brY].y - this.indexedRows[tlY].y + this.indexedRows[brY].height + 'px',
+                                width: this.indexedCols[brX].x - this.indexedCols[tlX].x + this.indexedCols[brX].width + 'px'
+                            },
+                            ...this.noBottomBorder ? { borderBottom: 'none' } : {},
+                            ...this.noRightBorder ? { borderRight: 'none' } : {}
                         }
                     });
                 });
@@ -51,44 +55,44 @@
             }
         },
         updated: function(){
-            console.log('selection-area update');
+            console.debug('selection-area update');
         }
     };
 </script>
 
 <style lang="less">
 .selection-area-root{
-    //position: relative; //GF:Rview: clear all the positions.
-    position: absolute;
-    top: -20px;  //GF:Rview: this should be the rowHeight * countOfHeaderRow
+    //GF:Review - suppose to be this way, for simpliciy, use fixed for now (below)
+    // position: absolute;
+    // top: -20px; 
+
+    position: fixed; //GF:Review - todo: I think absolute is better, but we need to set the top dynamically base on the height.
+    top: 0;  
     left: 0;
-    width: 0px;
-    height: 0px;
 
     .selection-area{
         --selection-area-color: rgba(255,99,88,0.25);
         --selection-area-border: #ff6358;
         
+        pointer-events: none;
         position: absolute;
         box-sizing: border-box;
-        pointer-events: none;
         border: 2px solid var(--selection-area-border);
         background-color: var(--selection-area-color);
 
-        .dot{
+        .drag-dot{
             position: absolute;
             bottom: -4px;
             right: -4px;
             height: 8px;
             width: 8px;
             border-radius: 4px;
-            background-color: #ff6358;
+            background-color: var(--selection-area-border);
             border: 0.5px solid white;
             pointer-events: all;
             cursor: crosshair;
         }
     }
-
 }
   
 </style>
