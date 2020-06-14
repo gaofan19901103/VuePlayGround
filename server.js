47633cf8783adc89693a10dace18c87bca1cd7a9
Gao Fan <gaofan19901103@gmail.com>
2020-06-14 21:34:26 +0800

    change the server port

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var app = express();
 app.use(express.static('dist'));
 
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('listening on port 3000'));
